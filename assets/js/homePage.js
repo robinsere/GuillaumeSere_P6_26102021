ee3342f08e9a46f05dd12c15bb221b3f708ed979
Robin SERE <r.sere@blackcover.fr>
2021-12-19 14:14:55 +0100

    filtre toggle

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -7,33 +7,30 @@ export default class HomePage {
   }
   renderHomePage() {
       // Création NavBar tags
-      const tags = vueTag.getTags(this.datas),
-        navTagsElement = document.getElementById("tags");
+      const tags = vueTag.getTags(this.datas);
+      this.navTagsElement = document.getElementById("tags");
 
-      navTagsElement.innerHTML = vueTag.createListTemplate(tags);
+      this.navTagsElement.innerHTML = vueTag.createListTemplate(tags);
 
       // Liste des profils
       const photographersListElement = document.getElementById("photographers-list");
       photographersListElement.innerHTML = vuePhotographersList.createListTemplate(this.datas.photographers);
 
       // Evenement selection Tag
-      navTagsElement.querySelectorAll("li").forEach((tag) => {
+      this.navTagsElement.querySelectorAll("li").forEach(tag => {
         tag.addEventListener("click", () => {
-     // Filtre les photographes qui contiennent le tag selectionné
-      const photographersSel = this.datas.photographers.filter(
-        (photographer) => photographer.tags.indexOf(tag.dataset.value) !== -1);
-        if (tag.classList.toggle('active')){
-    // Liste des profils filtrés
-        photographersListElement.innerHTML = vuePhotographersList.createListTemplate(photographersSel);
-        }
-     // Reset la liste des tags   
-        const tagsReset = document.getElementById("tag-item");
-        tagsReset.addEventListener("click", () =>{
-          photographersListElement.innerHTML = vuePhotographersList.createListTemplate(this.datas.photographers);
-          tag.classList.remove('active');
-        })
-
+          tag.classList.toggle('active');
+          // Filtre les photographes qui contiennent le tag selectionné
+          this.selPhotographe();
         });
       });
+  }
+  selPhotographe() {
+    let photographSel = this.datas.photographers;
+    const photographersListElement = document.getElementById("photographers-list");
+    this.navTagsElement.querySelectorAll("li.active").forEach(tag => {
+      photographSel =  [... this.datas.photographers.filter(photographer => photographer.tags.indexOf(tag.dataset.value) !== -1)];
+    });
+    photographersListElement.innerHTML = vuePhotographersList.createListTemplate(photographSel);
   }
 }  
